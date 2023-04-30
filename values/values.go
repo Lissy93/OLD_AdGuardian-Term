0d6bc482664388542e7f1c4b0237be3e1273aa9b
Alicia Sykes <alicia@omg.lol>
2023-04-30 22:33:05 +0100

    Adds file for constants

---
+++ values/values.go
@@ -0,0 +1,12 @@
+package values
+
+const (
+	// The URL of the AdGuard Home instance
+	ENDPOINT = ""
+	// A username with read-only access to the AdGuard Home instance
+	USERNAME = ""
+	// The password for the above user
+	PASSWORD = ""
+	// The application title, displayed top-left
+	TITLE = "AdGuard"
+)
